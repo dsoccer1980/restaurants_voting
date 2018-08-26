61bb18a5c6485544c697152e582376913940a4f8
dsoccer1980 <dsoccer1980@gmail.com>
2018-08-26 18:17:17 +0300

    Add Restaurant[Admin]RestController

---
+++ src/main/java/ru/dsoccer1980/web/restaurant/RestaurantAdminRestController.java
@@ -0,0 +1,48 @@
+package ru.dsoccer1980.web.restaurant;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.dsoccer1980.model.Restaurant;
+import ru.dsoccer1980.service.RestaurantService;
+
+import java.net.URI;
+
+
+@RestController
+@RequestMapping(value = RestaurantAdminRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestaurantAdminRestController {
+
+    static final String REST_URL = "/rest/admin/restaurants";
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Restaurant> createWithLocation(@RequestBody Restaurant restaurant) {
+        Restaurant created = restaurantService.create(restaurant);
+
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") int id) {
+        restaurantService.delete(id);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody Restaurant restaurant) {
+        restaurantService.update(restaurant);
+    }
+
+
+}
--- src/main/java/ru/dsoccer1980/web/restaurant/RestaurantRestController.java
+++ src/main/java/ru/dsoccer1980/web/restaurant/RestaurantRestController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.dsoccer1980.model.Restaurant;
-import ru.dsoccer1980.service.DishService;
 import ru.dsoccer1980.service.RestaurantService;
 
 import java.util.List;
@@ -20,13 +19,15 @@ public class RestaurantRestController {
     @Autowired
     private RestaurantService restaurantService;
 
-    @Autowired
-    private DishService dishService;
-
     @GetMapping
     public List<Restaurant> getAll() {
-        List<Restaurant> all = restaurantService.getAll();
-        return all;
+        return restaurantService.getAll();
     }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Restaurant get(@PathVariable("id") int id) {
+        return restaurantService.get(id);
+    }
+
 
 }
