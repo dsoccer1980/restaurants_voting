ac45fe9af4aadfa522e9253d5ff9eb352e50eddc
dsoccer1980 <dsoccer1980@gmail.com>
2018-09-30 16:32:37 +0300

    Add ResponseStatus to VoteException

--- src/main/java/ru/dsoccer1980/util/exception/VoteException.java
+++ src/main/java/ru/dsoccer1980/util/exception/VoteException.java
@@ -1,5 +1,9 @@
 package ru.dsoccer1980.util.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.METHOD_NOT_ALLOWED, reason = "Too late for vote")
 public class VoteException extends RuntimeException {
 
     public VoteException(String message) {
